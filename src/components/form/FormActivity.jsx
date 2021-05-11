41022bedab006a65240f35d24d04ad0252ff9588
Pierre <lightpower@hotmail.fr>
2021-05-11 19:59:12 +0200

    some FORMS

---
+++ src/components/form/FormActivity.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import "./Form.scss";
+
+export default function FormActivity() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [person, setPerson] = useState("");
+  const [img1, setImg1] = useState("");
+  const [img2, setImg2] = useState("");
+  const [img3, setImg3] = useState("");
+  const [img4, setImg4] = useState("");
+  const [location, setLocation] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <h1>Create Activity</h1>
+
+        <label>
+          Name :
+          <input
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          Description :
+          <textarea
+            name="description"
+            type="textarea"
+            rows="2"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          Persons :
+          <input
+            name="person"
+            type="number"
+            value={person}
+            onChange={(e) => setPerson(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Images URL :
+          <div className="img-row">
+            <input
+              name="img1"
+              type="url"
+              value={img1}
+              onChange={(e) => setImg1(e.target.value)}
+            />
+            {
+              <div className="preview-img">
+                {img1 && <img src={img1} alt="image 1" />}
+              </div>
+            }
+            <input
+              name="img2"
+              type="url"
+              value={img2}
+              onChange={(e) => setImg2(e.target.value)}
+            />
+            <div className="preview-img">
+              {img2 && <img src={img2} alt="image 2" />}
+            </div>
+          </div>
+          <div className="img-row">
+            <input
+              name="img3"
+              type="url"
+              value={img3}
+              onChange={(e) => setImg3(e.target.value)}
+            />
+            <div className="preview-img">
+              {img3 && <img src={img3} alt="image 3" />}
+            </div>
+            <input
+              name="img4"
+              type="url"
+              value={img4}
+              onChange={(e) => setImg4(e.target.value)}
+            />
+            <div className="preview-img">
+              {img4 && <img src={img4} alt="image 4" />}
+            </div>
+          </div>
+        </label>
+
+        <label>
+          Location :
+          <input
+            name="location"
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            required
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
