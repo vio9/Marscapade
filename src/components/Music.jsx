d98a9270ecadc0368c77cd39caf956d9d7c95cd9
Pierre <lightpower@hotmail.fr>
2021-05-12 00:05:37 +0200

    lofi easy

---
+++ src/components/Music.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import ptison from "../assets/music.mp3";
+
+export default function Music() {
+  return (
+    <>
+      <figcaption
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          position: "absolute",
+          right: "40%",
+          margin: "0",
+        }}
+      >
+        <audio
+          controls
+          loop="true"
+          autoPlay="true"
+          src={ptison}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+      </figcaption>
+    </>
+  );
+}
