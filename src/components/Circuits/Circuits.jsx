9a83b827efd2f1f2bb3fc1de66c3d7d3b170e28f
Pierre <lightpower@hotmail.fr>
2021-05-12 12:48:33 +0200

    smol Wall-e bot

--- src/components/Circuits/Circuits.jsx
+++ src/components/Circuits/Circuits.jsx
@@ -1,49 +1,50 @@
+import WallE from "../WallE/WallE";
 import React from "react";
 import CardCircuit from "./CardCircuit";
 import axios from "axios";
 import "./Circuits.scss";
 import { useState, useEffect } from "react";
 
-export default function Circuits () {
-    
-    const [circuitsList, setCircuitsList] = useState([])
-    const [loading, setLoading] = useState(true)
+export default function Circuits() {
+  const [circuitsList, setCircuitsList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getCircuitsList = async () => {
-            try {
-                const circuitsList = await axios.get('http://localhost:8080/circuits')
-                setCircuitsList(circuitsList.data)
-            } catch(err) {
-                console.log(err)
-            } finally {
-                setLoading(false)
-            }
-        }
-        getCircuitsList()
-    }, [])
+  useEffect(() => {
+    const getCircuitsList = async () => {
+      try {
+        const circuitsList = await axios.get("http://localhost:8080/circuits");
+        setCircuitsList(circuitsList.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getCircuitsList();
+  }, []);
 
-    console.log(circuitsList)
+  console.log(circuitsList);
 
-    if(loading) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>;
 
-    return (
-        <div className="allCircuitsElements">
-            <div className="titleContainer">
-                <h1>LES CIRCUITS</h1>
-            </div>
-            <div className="cardCircuitsContainer">
-                {circuitsList.map((circuit) =>(
-                    <CardCircuit
-                    id={circuit.id}
-                    circuitName={circuit.title}
-                    picture={circuit.image_1}
-                    distance={circuit.distance}
-                    duration={circuit.duration}
-                    difflevel={circuit.difflevel}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="allCircuitsElements">
+      <WallE />
+      <div className="titleContainer">
+        <h1>LES CIRCUITS</h1>
+      </div>
+      <div className="cardCircuitsContainer">
+        {circuitsList.map((circuit) => (
+          <CardCircuit
+            id={circuit.id}
+            circuitName={circuit.title}
+            picture={circuit.image_1}
+            distance={circuit.distance}
+            duration={circuit.duration}
+            difflevel={circuit.difflevel}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
