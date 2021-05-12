a9a8e740a3e0d4b61c429109e015ceecf822a4c0
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-05-12 02:17:35 +0200

    dashboard delete

--- src/components/Dashboard/DashActivityListing.jsx
+++ src/components/Dashboard/DashActivityListing.jsx
@@ -2,9 +2,10 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import {CircularProgress} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
-export default function() {
+export default function DashActivityListing() {
 
     const [activityList, setActivityList] = useState([])
     const [loading, setLoading] = useState(true)
@@ -26,19 +27,34 @@ export default function() {
         
     }, [loading])
 
+    const handleDeleteActivity = (id) => {
+        const deleteCircuit = async () => {
+            try{
+                const response = await axios.delete(`http://localhost:8080/activities/${id}`)
+                setActivityList(activityList.filter((activity) => activity.id !== id))
+            } catch(err) {
+                console.log(err)
+            }   
+        }
+        deleteCircuit()
+    }
+
     console.log('activities', activityList)
     if(loading) return <CircularProgress />
+
+    console.log('dash activity', activityList)
 
     return(
 
         activityList && activityList.length > 0 && (
-            activityList.map((act) => {
+            activityList.map((act, index) => {
 
-                <div>
+                return ( <div key={index}>
                     <div>Activity #{act.id}</div><div>img: {act.image_1}</div><div>activity title{act.title}</div>
                     <div>Activity Desc: {act.description}</div><div>Price: {'pas chere'}</div><div>Location: {act.location}</div><div>Nbr de participant: {act.nbpax}</div>
-
-                </div>
+                    <div><DeleteIcon onClick={() => handleDeleteActivity(act.id)}/></div>
+                    </div>
+                )
 
             }
         )
