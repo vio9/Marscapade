32d40a139bd76a376c6889fc8dd343b18c90d749
Pierre <lightpower@hotmail.fr>
2021-05-12 13:31:20 +0200

    fix url issue

--- src/components/WallE/WallE.jsx
+++ src/components/WallE/WallE.jsx
@@ -5,7 +5,7 @@ import wallePng from "../../assets/wall-e.png";
 export default function WallE() {
   return (
     <div>
-      <a href="localhost:8080" className="wall-e">
+      <a href="http://localhost:8080" className="wall-e">
         <p>Need help ?</p>
         <span></span>
         <img src={wallePng} alt="wall-e the bot"></img>
